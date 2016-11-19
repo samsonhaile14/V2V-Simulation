b5cb80e71d516cade2c272e7e3c050bb34acf93f
Nicole Ortogero <trptman20@gmail.com>
2016-11-19 11:34:03 -0800

    rays commit

---
+++ V2V_sim/Assets/udp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class udp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		int srcIP = 0;
+		int destIP = 65535;
+		int srcPort = 0;
+		int destPort = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
