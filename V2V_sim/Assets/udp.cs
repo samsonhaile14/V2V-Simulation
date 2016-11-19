5ae54d689cea1eea61a77446be3f02c29b4afc9e
Nicole Ortogero <trptman20@gmail.com>
2016-11-19 12:14:18 -0800

    some script stuff

--- V2V_sim/Assets/udp.cs
+++ V2V_sim/Assets/udp.cs
@@ -6,15 +6,26 @@ public class udp : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		int srcIP = 0;
-		int destIP = 65535;
-		int srcPort = 0;
-		int destPort = 0;
+		int srcIP = 0;       // static IP addresses 
+		int destIP = 65535;  // broadcast address
+		int srcPort = 80;
+		int destPort = 80;
+		int speed = 0;
+		string direction = north;
+		char congestionLevel = 0;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// select route from available paths
+
+		// detect closest car in route
+
+		// broadcast in angle relative to velocity
+
+		//  
 
 	
 	}
